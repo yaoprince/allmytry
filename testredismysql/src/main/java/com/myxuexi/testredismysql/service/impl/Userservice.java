962eb741af8e30e502fe3fe7bf40dd93a3d302f8
yaoshuli <2905715322@qq.com>
2019-11-03 22:12:55 +0800

    主干第二次提交

---
+++ testredismysql/src/main/java/com/myxuexi/testredismysql/service/impl/Userservice.java
@@ -0,0 +1,10 @@
+package com.myxuexi.testredismysql.service.impl;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Userservice {
+    public String gogi(){
+        return "qweqwieqw";
+    }
+}
