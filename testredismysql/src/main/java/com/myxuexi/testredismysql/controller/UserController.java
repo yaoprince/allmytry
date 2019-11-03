349f1c36f676cadaba4d6774171d15a3b540718a
yaoshuli <2905715322@qq.com>
2019-11-03 23:11:00 +0800

    心增加

---
+++ testredismysql/src/main/java/com/myxuexi/testredismysql/controller/UserController.java
@@ -0,0 +1,9 @@
+package com.myxuexi.testredismysql.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/test")
+@RestController
+public class UserController {
+}
